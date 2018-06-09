a534138d76950c267f6943aa66f2d776694a0b63
Sportihome sas <sportihomesas@pc-80.home>
2018-06-09 19:56:26 +0200

    search improuved and search list added

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ContentDrawerComponent } from './content-drawer/content-drawer';
+import { ShrinkingSegmentHeaderComponent } from './shrinking-segment-header/shrinking-segment-header';
 import { IonicModule  } from 'ionic-angular';
 // import { ExpandableHeaderComponent } from './expandable-header/expandable-header';
+
 @NgModule({
-	declarations: [ContentDrawerComponent],
+	declarations: [ContentDrawerComponent,
+    ShrinkingSegmentHeaderComponent],
 	imports: [IonicModule],
-	exports: [ContentDrawerComponent]
+	exports: [ContentDrawerComponent,
+    ShrinkingSegmentHeaderComponent]
 })
 export class ComponentsModule {}
