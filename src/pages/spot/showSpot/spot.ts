176dc62922bb2964c2cd407363d1627dde1fa7e7
AdrienChassagne <achassagnepro@gmail.com>
2018-06-13 17:33:30 +0200

    ad gogo

--- src/pages/spot/showSpot/spot.ts
+++ src/pages/spot/showSpot/spot.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild, NgZone, OnDestroy } from '@angular/core';
-import { App, IonicPage, NavController, NavParams, Platform, Content } from 'ionic-angular';
+import { App, IonicPage, NavController, NavParams, Platform, Content, ModalController } from 'ionic-angular';
+import { LoginPage } from '../../../pages/login/login'
 
-import { SpotsProvider, PlacesProvider } from '../../../providers/providers';
+import { SpotsProvider, PlacesProvider, AuthProvider, User } from '../../../providers/providers';
 import { Observable, Subject } from 'rxjs';
 import 'rxjs/add/operator/takeUntil';
 
@@ -19,14 +20,21 @@ export class SpotPage implements OnDestroy {
   showNavbar: boolean = false;
   sliderHeight: number = 0;
   scrolled: boolean = false;
+  favorite: boolean;
+  imageChange$ = new Subject();
+  defaultImage: any;
   private ngUnsubscribe: Subject<any> = new Subject();
 
-  constructor(public appCtrl: App, public navCtrl: NavController, private zone: NgZone, public spotsProvider: SpotsProvider, public placesProvider: PlacesProvider, public navParams: NavParams, public platform: Platform) {
+  constructor(public modalCtrl: ModalController, public userProvider: User, public authProvider: AuthProvider, public appCtrl: App, public navCtrl: NavController, private zone: NgZone, public spotsProvider: SpotsProvider, public placesProvider: PlacesProvider, public navParams: NavParams, public platform: Platform) {
     this.sliderHeight = this.platform.height() * 0.4 + 40;
   }
 
   showUser(id) {
     this.appCtrl.getRootNav().push('UserProfilePage', { id: id });
+  } auth
+
+  slideChanged() {
+    this.imageChange$.next(1000);
   }
 
   scrolledToggle() {
@@ -40,7 +48,7 @@ export class SpotPage implements OnDestroy {
       .takeUntil(this.ngUnsubscribe)
       .subscribe(data => {
         this.spot = data;
-
+        this.defaultImage = "'https://test.sportihome.com/uploads/places/'" + this.spot._id + '/large/' + this.spot.pictures[0];
         this.spotsProvider.getComments(this.spot._id)
           .takeUntil(this.ngUnsubscribe)
           .subscribe(comments => this.comments = comments);
@@ -53,6 +61,27 @@ export class SpotPage implements OnDestroy {
           .takeUntil(this.ngUnsubscribe)
           .subscribe(spotsNearBy => this.spotsNearBy = spotsNearBy);
       });
+  }
+
+  setFavoriteStatus(event, placeId) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.authProvider.getLogStatus() === true) {
+      if (this.userProvider.isFavoritePlace(placeId) === false) {
+        this.favorite = true;
+        this.userProvider.addPlaceFavorite(placeId)
+          .takeUntil(this.ngUnsubscribe)
+          .subscribe();
+      } else {
+        this.favorite = false;
+        this.userProvider.removePlaceFavorite(placeId)
+          .takeUntil(this.ngUnsubscribe)
+          .subscribe();
+      }
+    } else {
+      let loginModal = this.modalCtrl.create(LoginPage);
+      loginModal.present();
+    }
   }
 
   ngOnDestroy() {
